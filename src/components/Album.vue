4abceaf4f46ce61631c6ed46b7d7a0e88b3bfe1c
lucaperini <luca.perini@gmail.com>
2022-07-07 10:33:47 +0200

    fix css

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <div class="row flex-column">
+    <div class="d-flex row">
       <div class="col-12 text-center pb-3">
-        <div class="card rounded" style="width: 14rem">
+        <div class="card rounded">
           <img
             class="card-img-top p-3"
             :src="albumElement.poster"
@@ -37,16 +37,27 @@ export default {
 <style lang="scss" scoped>
 .card {
   background-color: rgb(48, 58, 69);
-  height: 380px;
+  height: 350px;
   padding-bottom: 0.5rem;
+  transition: border 1s ease-in-out;
+  width: 14rem;
 }
+
+.card:hover {
+  border: 1px solid rgba(255, 255, 255, 0.5);
+  padding-bottom: 0.5rem;
+  box-shadow: 0px 0px 10px 0px rgba(255, 255, 255, 0.3);
+}
+
 .card-body {
   padding: 0.2rem;
+  margin-left: 0;
 }
 .card-body h5 {
   padding: 0.3rem;
-  font-size: 1.3rem;
+  font-size: 1rem;
 }
+
 h5 {
   color: white;
   text-transform: uppercase;
@@ -54,8 +65,9 @@ h5 {
   font-weight: bold;
   font-size: 1.2rem;
 }
+
 .card-title h5 {
-  line-height: 0.8rem;
+  line-height: 1.2rem;
 }
 
 p {
--- src/components/AlbumList.vue
+++ src/components/AlbumList.vue
@@ -1,73 +1,84 @@
 <template>
-    <main>
-        <div class="container p-5 gap-1">
-            <div class="d-flex row row-cols-3 align-content-center">
-                <Album
-                    v-for="(element, index) in filteredDiscsList" 
-                    :key="index" 
-                    :albumElement="element"
-                />
-            </div>
-        </div>
-    </main>
+  
+    <div class="container p-5">
+      <div class="row row-cols-3">
+        
+      
+        <Album
+          class="album"
+          v-for="(element, index) in filteredDiscsList"
+          :key="index"
+          :albumElement="element"
+        />
+    
+            
+    
+        
+      </div>
+    </div>
+  
 </template>
 <script>
 import axios from "axios";
-import Album from './Album.vue';
+import Album from "./Album.vue";
 
 export default {
-    name: "AlbumList",
-    props: ['selectedGenre'],
-    components: {
-        Album,
-    },  
-    data: function(){
-        return {
-            albumList : null,
-            genresList : [],
-            }
-        
-    },
-    
-    computed: {
-        filteredDiscsList(){
-            if(this.selectedGenre === ''){
-                return this.albumList;
-            }
-            return this.albumList.filter(
-                (element) => element.genre === this.selectedGenre)
-        }
-    },
+  name: "AlbumList",
+  props: ["selectedGenre"],
+  components: {
+    Album,
+  },
+  data: function () {
+    return {
+      albumList: null,
+      genresList: [],
+    };
+  },
 
-    created(){
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/array/music')
-            .then((result) => {
-                
-                this.albumList = result.data.response;
-                
-                this.albumList.forEach((discElement) => {
-                    if (!this.genresList.includes(discElement.genre)){
-                        this.genresList.push(discElement.genre)
-                    }
-                })
-                this.$emit('loadedGenres', this.genresList);
-            })
-            .catch((error) => {
-                console.error(error);
-            })
-    }
-} 
+  computed: {
+    filteredDiscsList() {
+      if (this.selectedGenre === "") {
+        return this.albumList;
+      }
+      return this.albumList.filter(
+        (element) => element.genre === this.selectedGenre
+      );
+    },
+  },
+
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((result) => {
+        this.albumList = result.data.response;
+
+        this.albumList.forEach((discElement) => {
+          if (!this.genresList.includes(discElement.genre)) {
+            this.genresList.push(discElement.genre);
+          }
+        });
+        this.$emit("loadedGenres", this.genresList);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 
-main {
-    
-    height: 100%
-}
 .container {
-    max-width:850px;
+    max-width:800px;
+    display: flex;
+    direction: row;
+    flex-wrap: wrap;
     
 }
+
+.album {
+    margin-left:0;
+}
+
+
 </style>
